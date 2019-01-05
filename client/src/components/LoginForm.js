325695964c072be0b2f66b75459351d181d12799
Ester Vojkollari <e.vojko@gmail.com>
2019-01-05 17:57:33 -0500

    minor login form layout fixes

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,32 +1,37 @@
 import React from 'react';
-import { Button, Form, Image} from 'semantic-ui-react'
+import { Grid, Button, Form, Image} from 'semantic-ui-react'
+import './LoginForm.css'
 
 function LoginForm(props){
   return (
-    <Form  style={{ maxWidth: 450 }} onSubmit={props.handleLogin}>
-      <h2>Login</h2>
-      <h5>Email</h5>
-      <Form.Input
-             fluid
-             icon='user'
-             iconPosition='left'
-             placeholder='E-mail address'
-             onChange={props.handleChange}
-             name="email"
-             value={props.login.email}/>
-      <br></br>
-      <h5>Password</h5>
-       <Form.Input
-              fluid
-              icon='lock'
-              iconPosition='left'
-              placeholder='Password'
-              type='password' onChange={props.handleChange}
-              name="password"
-              value={props.login.password}/>
-      <br></br>
-      <Button color='teal' fluid size='large' type="submit">Login</Button>
-    </Form>
+    <div className="login">
+      <Grid style={{ height: '100%' }} verticalAlign='middle' centered>
+        <Grid.Row centered>
+          <Form style={{ width: 450}} onSubmit={props.handleLogin}>
+            <h2>Login</h2>
+            <h5>Email</h5>
+            <Form.Input
+                   fluid
+                   icon='user'
+                   iconPosition='left'
+                   placeholder='E-mail address'
+                   onChange={props.handleChange}
+                   name="email"
+                   value={props.login.email}/>
+            <h5>Password</h5>
+             <Form.Input
+                    fluid
+                    icon='lock'
+                    iconPosition='left'
+                    placeholder='Password'
+                    type='password' onChange={props.handleChange}
+                    name="password"
+                    value={props.login.password}/>
+            <Button color='teal' fluid size='large' type="submit">Login</Button>
+          </Form>
+        </Grid.Row>
+      </Grid>
+    </div>
   )
 }
 
