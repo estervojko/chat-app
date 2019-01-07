1350e918dc5e4fb5f118d34c78fdbfcfd0f0a10a
Ester Vojkollari <e.vojko@gmail.com>
2019-01-07 15:36:11 -0500

    fixed urls for deployment

--- client/src/services/url.js
+++ client/src/services/url.js
@@ -1,5 +1,5 @@
-export const API_ROOT = 'http://localhost:3000/api';
-export const API_WS_ROOT = 'ws://localhost:3001/api/cable';
+export const API_ROOT = 'https://chat-app-ester.herokuapp.com/api';
+export const API_WS_ROOT = 'ws://chat-app-ester.herokuapp.com/api/cable';
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
