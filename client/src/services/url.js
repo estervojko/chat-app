1c1ed115c016ac83e7927dbcdb4230182e8d98b2
Ester Vojkollari <e.vojko@gmail.com>
2019-01-07 15:41:32 -0500

    minor fix changing from ws to wss protocol

--- client/src/services/url.js
+++ client/src/services/url.js
@@ -1,5 +1,5 @@
 export const API_ROOT = 'https://chat-app-ester.herokuapp.com/api';
-export const API_WS_ROOT = 'ws://chat-app-ester.herokuapp.com/api/cable';
+export const API_WS_ROOT = 'wss://chat-app-ester.herokuapp.com/api/cable';
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
