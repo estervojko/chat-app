5814c787b7d95acc12b77a96bf8e430b5f657cef
Ester Vojkollari <e.vojko@gmail.com>
2019-01-09 10:51:25 -0500

    improved grid layout with semantic ui

--- client/src/components/Chatroom.js
+++ client/src/components/Chatroom.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import MessageList from './MessageList'
 import MessageForm from './MessageForm'
 
+import { Container, Segment, Grid, List } from 'semantic-ui-react'
+
+
 import { ActionCable } from 'react-actioncable-provider';
 
 
@@ -9,16 +12,36 @@ class Chatroom extends Component{
   render(){
     return(
       <div>
-        <h2>Welcome to the chatroom</h2>
-        {this.props.chatrooms.map(room => (
-          <p key={room.id} onClick={() => {this.props.loadChatMessages(room.id)}}>{room.title}</p>
-        ))}
-        <MessageList messages={this.props.messages}/>
-        <ActionCable channel={{channel: "MessagesChannel"}}
-                     onReceived={this.props.handleReceivedMessage}/>
-        <MessageForm handleMessageChange={this.props.handleMessageChange}
-                     message={this.props.message}
-                     handleMessageSubmit={this.props.handleMessageSubmit}/>
+        <Container>
+          <h2>Welcome to the chatroom</h2>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column width={4} stretched>
+                <Segment raised>
+                  <List animated verticalAlign='middle'>
+                    {this.props.chatrooms.map(room => (
+                      <List.Item>
+                        <List.Content>
+                          <List.Header key={room.id} onClick={() => {this.props.loadChatMessages(room.id)}}>{room.title}</List.Header>
+                        </List.Content>
+                      </List.Item>
+                    ))}
+                  </List>
+                </Segment>
+              </Grid.Column>
+              <Grid.Column width={12}>
+                <Segment raised>
+                  <MessageList messages={this.props.messages}/>
+                  <ActionCable channel={{channel: "MessagesChannel"}}
+                               onReceived={this.props.handleReceivedMessage}/>
+                  <MessageForm handleMessageChange={this.props.handleMessageChange}
+                               message={this.props.message}
+                               handleMessageSubmit={this.props.handleMessageSubmit}/>
+                </Segment>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
       </div>
     )
   }
--- client/src/components/MessageList.js
+++ client/src/components/MessageList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { Icon, Feed } from 'semantic-ui-react'
+
 export default class MessageList extends Component{
   constructor(props){
     super()
@@ -12,6 +14,26 @@ export default class MessageList extends Component{
         {this.props.messages.map(msg => (
           <p key={msg.id}>{msg.content}</p>
         ))}
+        <Feed.Event>
+          <Feed.Label image='/images/avatar/small/joe.jpg' />
+          <Feed.Content>
+            <Feed.Summary>
+              <a>Joe Henderson</a> posted on his page
+              <Feed.Date>3 days ago</Feed.Date>
+            </Feed.Summary>
+            <Feed.Extra text>
+              Ours is a life of constant reruns. We're always circling back to where we'd we started,
+              then starting all over again. Even if we don't run extra laps that day, we surely will
+              come back for more of the same another day soon.
+            </Feed.Extra>
+            <Feed.Meta>
+              <Feed.Like>
+                <Icon name='like' />
+                5 Likes
+              </Feed.Like>
+            </Feed.Meta>
+          </Feed.Content>
+        </Feed.Event>
       </div>
     )
   }
